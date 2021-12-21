96ef85e05265980c1de02026b46fb312da034fab
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-12-21 22:32:56 +0900

    npm tailwind.css 설치하기

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,8 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Laravel')</title>
+    <link rel="stylesheet" href="{{ mix('css/tailwind.css') }}">
 </head>
 <body>
+    <div class="bg-red-800">hello</div>
     <ul>
         <li><a href="/">Welcome</a></li>
         <li><a href="/hello">Hello</a></li>
