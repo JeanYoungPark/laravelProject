56867e63d0c77355c6212f49e2ffa51f27806c57
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-12-24 02:25:10 +0900

    태스크 삭제

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,7 +4,10 @@
 
 @section('content')
     <div class="px-64">
-        <h1 class="font-bold text-3xl">Tasks</h1>
+        <div class="flex pt-2">
+            <h1 class="font-bold text-3xl flex-1">Tasks</h1>
+            <a href="/tasks/create"><button class="bg-gray-300 hover:bg-gray-400 px-4 py-2">Create Task</button></a>
+        </div>
         <ul>
             @foreach ($tasks as $task)
                 <li class="border p-2 my-2"><a class="block" href="/tasks/{{ $task->id }}">Title: {{ $task->title }}<small class="float-right">Created at {{ $task->created_at }}</small></a></li>
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -6,7 +6,16 @@
     <div class="px-64">
         <div class="flex py-2">
             <h1 class="font-bold text-3xl flex-1">Show Task</h1>
-            <a href="/tasks/{{ $task->id }}/edit"><button class="flex-initial bg-gray-300 px-2 hover:bg-gray-500">Edit</button></a>
+            <div class="flex-initial">
+                <a href="/tasks/{{ $task->id }}/edit">
+                    <button class="bg-gray-300 px-2 hover:bg-gray-500">Edit</button>
+                </a>
+                <form action="/tasks/{{ $task->id }}" method="post" class="float-right ml-1">
+                    @method('delete')
+                    @csrf
+                    <button class="bg-red-300 px-2 hover:bg-gray-500">Delete</button>
+                </form>
+            </div>
         </div>
         Title: {{ $task->title }}
         <small class="float-right">Created at {{ $task->created_at }}</small><br>
