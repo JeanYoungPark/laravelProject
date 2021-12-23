54284941c109092e069b1ccbd43f3edc00c08455
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-12-24 02:56:57 +0900

    폼 기존 값 유지

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -8,13 +8,13 @@
         <form action="/tasks" method="post">
             @csrf
             <label class="block" for="title">Title</label>
-            <input class="@error('title') border border-red-500 @enderror border border-gray-800 w-full" type="text" name="title" id="title" required>
+            <input class="@error('title') border border-red-500 @enderror border border-gray-800 w-full" type="text" name="title" id="title" required value="{{ old('title') ? old('title') : '' }}">
             <br>
             @error('title')
                 <small class="text-red-500">{{ $message }}</small>
             @enderror
             <label class="block" for="body">Body</label>
-            <textarea class="@error('body') border border-red-500 @enderror border border-gray-800 w-full" name="body" id="body" cols="30" rows="10" required></textarea>
+            <textarea class="@error('body') border border-red-500 @enderror border border-gray-800 w-full" name="body" id="body" cols="30" rows="10" required>{{ old('body') ? old('body') : '' }}</textarea>
             <br>
             @error('body')
                 <small class="text-red-500">{{ $message }}</small>
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -9,13 +9,13 @@
             @method('put')
             @csrf
             <label class="block" for="title">Title</label>
-            <input class="@error('title') border border-red-500  @enderror border border-gray-800 w-full" type="text" name="title" id="title" value="{{ $task->title }}" required>
+            <input class="@error('title') border border-red-500  @enderror border border-gray-800 w-full" type="text" name="title" id="title" value="{{ old('title') ? old('title') : $task->title }}" required>
             <br>
             @error('title')
                 <small class="text-red-500">{{ $message }}</small>
             @enderror
             <label class="block" for="body">Body</label>
-            <textarea class="@error('body') border border-red-500  @enderror border border-gray-800 w-full" name="body" id="body" cols="30" rows="10" required>{{ $task->body }}</textarea>
+            <textarea class="@error('body') border border-red-500  @enderror border border-gray-800 w-full" name="body" id="body" cols="30" rows="10" required>{{ old('body') ? old('body') : $task->body }}</textarea>
             <br>
             @error('body')
                 <small class="text-red-500">{{ $message }}</small>
